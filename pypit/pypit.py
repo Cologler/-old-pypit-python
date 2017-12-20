6b0419d4fc353849241ff0dbebb81bf382f97c79
Cologler <skyoflw@gmail.com>
2017-12-21 07:23:49 +0800

    auto append history

--- pypit/pypit.py
+++ pypit/pypit.py
@@ -161,18 +161,39 @@ class PackageMetadata:
 
 
 def get_rst_doc():
-    if os.path.isfile('README.rst'):
-        print('[INFO] resolve description from README.rst.')
-        with open('README.rst') as fp:
-            return fp.read()
 
-    if os.path.isfile('README.md'):
-        print('[INFO] resolve description from README.md.')
-        with open('README.md') as fp:
-            return md2rst(fp.read())
+    def resolve_desc(name, converter):
+        if os.path.isfile(name):
+            print('[INFO] resolve description from {}.'.format(name))
+            with open(name) as fp:
+                content = fp.read()
+                return content if converter is None else converter(content)
 
-    print('[INFO] no description found.')
-    return ''
+    rst_doc = resolve_desc('README.rst', None) or resolve_desc('README.md', md2rst)
+
+    if rst_doc is None:
+        print('[INFO] no description found.')
+        return ''
+
+    assert isinstance(rst_doc, str)
+
+    # append history
+
+    def resolve_history(name, converter):
+        if os.path.isfile(name):
+            print('[INFO] append history from {}.'.format(name))
+            with open(name) as fp:
+                content = fp.read()
+                return content if converter is None else converter(content)
+
+    history = resolve_history('HISTORY.rst', None) or resolve_history('HISTORY.md', md2rst)
+
+    if history is None:
+        print('[INFO] no history found.')
+    else:
+        rst_doc = rst_doc + '\n\n' + history,
+
+    return rst_doc
 
 
 def pypit(projdir: str):
